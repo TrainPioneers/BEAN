e944f503adfd786d30395deae026086d84b4714a
ValentinoArtGallery <126825095+ValentinoArtGallery@users.noreply.github.com>
2024-10-31 20:09:03 -0400

    Simple stuff, setting up python and installing dep

---
+++ backend/sentiment/prototype/neuron.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+import numpy as np
+@dataclass
+class neuron:
+    w: np.array
+    x: np.array
---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+pip install ../../backend/sentiment/requirements.txt
+#I couldn't check if it works man
