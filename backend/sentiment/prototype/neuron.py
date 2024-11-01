edc3467bcc7eb06c298b5ee7f69009220850eae7
Bruno Arpa <bruno.arpa.n@gmail.com>
2024-11-01 12:14:45 +0100

    yes neuron.py

--- backend/sentiment/prototype/neuron.py
+++ backend/sentiment/prototype/neuron.py
@@ -1,6 +1,16 @@
 from dataclasses import dataclass
 import numpy as np
 @dataclass
-class neuron:
-    w: np.array
-    x: np.array
+class Neuron:
+    w: np.array #weights
+    x: np.array 
+    b: float = 0.0  # bias 
+
+    def forward(self, x: np.array) -> float: #caluclate output of neuron and pass through activation function
+        z = np.dot(self.w, x) + self.b  # weighted sum + bias
+        return self.sigmoid(z)  # activation function
+
+    def sigmoid(self, z: float) -> float:
+        return 1 / (1 + np.exp(-z))
+
+    #need def for updating weights gradient descnet
