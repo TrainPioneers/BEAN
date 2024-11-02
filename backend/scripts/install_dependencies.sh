d4755df63d57b0c94df62fb61ea20193d43238bc
Scarlett Parker <95775118+scarlettmparker@users.noreply.github.com>
2024-11-02 21:10:40 +0000

    re-arranged backend files + set up front-end files
    feel free to move the scripts folder back but i think it should stay separate for front-end and backend and not be in the parent directory. it was still broken so i fixed it.
    
    set up a venv so any new packages can be frozen into requirements easily. will start a new branch for front-end stuff after this

---
+++ backend/scripts/install_dependencies.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+pip install -r ./requirements.txt
+#I couldn't check if it works man
--- scripts/install_dependencies.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-pip install -r ../../backend/sentiment/requirements.txt
-#I couldn't check if it works man
